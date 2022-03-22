4327dc0ebcde4c05d47818ff31207647ad24c966
fabrv <fabrizzio.rivera@telus.com>
2022-03-22 13:50:48 -0600

    feat: project swagger

--- controllers/project.go
+++ controllers/project.go
@@ -9,6 +9,17 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// Get Projects
+// @Summary Get All Projects
+// @Description Get all projects
+// @Tags Projects
+// @Produce json
+// @Param limit query number false "Limit"
+// @Param offset query number false "Offset"
+// @Param name query string false "Name"
+// @Param ids query string false "IDs"
+// @Success 200 {array} models.ProjectResponse
+// @Router /projects [get]
 func GetProjects(c *fiber.Ctx) error {
 	db := database.DBConn
 
@@ -36,19 +47,63 @@ func GetProjects(c *fiber.Ctx) error {
 	}
 
 	query.Find(&projects)
-	return c.JSON(projects)
+
+	var projectResponses []models.ProjectResponse
+	for _, project := range projects {
+		projectResponses = append(projectResponses, models.ProjectResponse{
+			ID:          project.ID,
+			Name:        project.Name,
+			Description: project.Description,
+			CreatedAt:   project.CreatedAt,
+			UpdatedAt:   project.UpdatedAt,
+		})
+	}
+
+	return c.JSON(projectResponses)
 }
 
+// Get Project
+// @Summary Get Project
+// @Description Get project
+// @Tags Projects
+// @Produce json
+// @Param id path string true "ID"
+// @Success 200 {object} models.ProjectResponse
+// @Error 404 {object} models.ErrorResponse
+// @Router /projects/{id} [get]
 func GetProject(c *fiber.Ctx) error {
 	id := c.Params("id")
 	db := database.DBConn
 	var project models.Project
 	db.First(&project, id)
-	return c.JSON(project)
+
+	if project.ID == 0 {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "Project not found",
+		})
+	}
+
+	return c.JSON(models.ProjectResponse{
+		ID:          project.ID,
+		Name:        project.Name,
+		Description: project.Description,
+		CreatedAt:   project.CreatedAt,
+		UpdatedAt:   project.UpdatedAt,
+	})
 }
 
+// Create Project
+// @Summary Create Project
+// @Description Create project
+// @Tags Projects
+// @Accept json
+// @Produce json
+// @Param project body models.ProjectPayload true "Project"
+// @Success 200 {object} models.ProjectResponse
+// @Error 500 {object} models.ErrorResponse
+// @Router /projects [post]
 func AddProject(c *fiber.Ctx) error {
-	var project models.Project
+	var project models.ProjectPayload
 	if err := c.BodyParser(&project); err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"error": err.Error(),
@@ -56,13 +111,16 @@ func AddProject(c *fiber.Ctx) error {
 	}
 
 	errors := utils.ValidateStruct(project)
-
 	if errors != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(errors)
 	}
 
 	db := database.DBConn
-	status := db.Create(&project)
+	projectModel := models.Project{
+		Name:        project.Name,
+		Description: project.Description,
+	}
+	status := db.Create(&projectModel)
 
 	if status.Error != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
@@ -72,28 +130,59 @@ func AddProject(c *fiber.Ctx) error {
 	return c.JSON(project)
 }
 
+// Update Project
+// @Summary Update Project
+// @Description Update project
+// @Tags Projects
+// @Accept json
+// @Produce json
+// @Param id path string true "ID"
+// @Param project body models.ProjectPayload true "Project"
+// @Success 200 {object} models.ProjectResponse
+// @Error 503 {object} models.ErrorResponse
+// @Router /projects/{id} [put]
 func UpdateProject(c *fiber.Ctx) error {
 	id := c.Params("id")
-	var project models.Project
+	var project models.ProjectPayload
 	if err := c.BodyParser(&project); err != nil {
 		return c.Status(503).JSON(fiber.Map{
 			"error": err.Error(),
 		})
 	}
+
+	errors := utils.ValidateStruct(project)
+	if errors != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(errors)
+	}
+
 	db := database.DBConn
-	db.Model(&project).Where("id = ?", id).Updates(project)
+	projectModel := models.Project{
+		Name:        project.Name,
+		Description: project.Description,
+	}
+	db.Model(&projectModel).Where("id = ?", id).Updates(projectModel)
 	return c.JSON(fiber.Map{
 		"message": "Project updated",
 	})
 }
 
+// Delete Project
+// @Summary Delete Project
+// @Description Delete project
+// @Tags Projects
+// @Accept json
+// @Produce json
+// @Param id path string true "ID"
+// @Success 200 {object} models.ProjectResponse
+// @Error 404 {object} models.ErrorResponse
+// @Router /projects/{id} [delete]
 func DeleteProject(c *fiber.Ctx) error {
 	id := c.Params("id")
 	db := database.DBConn
 	var project models.Project
 	db.First(&project, id)
 	if project.ID == 0 {
-		return c.Status(404).JSON(fiber.Map{
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 			"error": "Project not found",
 		})
 	}
